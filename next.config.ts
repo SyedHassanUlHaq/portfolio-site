15fe39a87607ddc5fdbe90286f8f2f91cb5a3a9d
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2025-07-24 02:23:54 +0500

    removed next export because deprecated

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export", // Required for static export
-  basePath: "/portfolio-site", // Set this to your repo name
-  assetPrefix: "/portfolio-site", // Ensures correct paths for static files
+// next.config.ts
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio-site",
+  assetPrefix: "/portfolio-site",
 };
 
 export default nextConfig;
