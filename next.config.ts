1da73e789616c70ebb826c04abf380f4224f6e24
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2025-07-24 01:40:45 +0500

    fixed image base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Required for static export
+  basePath: "/portfolio-site", // Set this to your repo name
+  assetPrefix: "/portfolio-site", // Ensures correct paths for static files
 };
 
 export default nextConfig;
